2a73c1052a372d6b695968e6a047b1d95d7e80eb
Brandon Vargo <brandon.vargo@gmail.com>
2010-02-21 14:51:01 -0700

    Partials support.
    Support for rendering partial templates from within another template.

--- modules/core/display/display.php
+++ modules/core/display/display.php
@@ -26,6 +26,7 @@ class Display
       $this->smarty->compile_dir     = smconfig_get('cache_dir') . 'smarty/';
       $this->smarty->cache_dir       = $this->smarty->compile_dir.'cache';
       $this->smarty->plugins_dir = array('plugins', $SM_FS_ROOT . 'smarty');
+      $this->smarty->default_template_handler_func = array('Display', 'smarty_get_template');
 
       // create the smarty cache directory if it doesn't exist
       makedir($this->smarty->compile_dir);
@@ -34,6 +35,8 @@ class Display
       $this->smarty->caching = false;
 
       // TODO: turn this off for production?
+      // checks all template files for modification time to see if 
+      // recompilation of the template is necessary
       $this->smarty->compile_check = true;
 
       // root directory for templates
@@ -50,6 +53,15 @@ class Display
    // fetches a template for the given module
    public function fetch_template($module, $template, $args=array())
    {
+      // ensure each module gets its own compiled template file, so two
+      // different modules using the same filename for an included template do 
+      // not get the wrong compiled template file.
+      // for example, if modules a and b both have a partial template _foo.tpl 
+      // under templates/a and templates/b, respectfully, then this will 
+      // produce compiled templates in the cache directory for each _foo.tpl, 
+      // instead of just one for the name _foo.tpl.
+      $this->smarty->compile_id = $module;
+
       $tpl = $this->prepare_template($module, $template, $args);
       return $this->smarty->fetch($tpl);
 	}
@@ -79,6 +91,7 @@ class Display
 		$this->smarty_assign('SM_SELF', $SM_SELF);
 		$this->smarty_assign('SM_ARGSTRING', implode('/', $SM_ARGS));
 		$this->smarty_assign('SM_RR', $SM_RR);
+		$this->smarty_assign('SM_MODULE', $module);
       if($SM_USER)
          $this->smarty_assign('SM_USER', $SM_USER);
 
@@ -86,27 +99,62 @@ class Display
       $this->smarty_assign($args);
 
       // validate that the template exists and is readable
-      $tpl = self::get_template(strtolower($module).'/'.strtolower($template));
-
+      // locate a template specific to this module
+      $tpl = self::get_template($template, $module);
+      // if both have failed, throw an error
       if($tpl === NULL)
-      {
 			error("Invalid template `$tpl` passed to Display");
-      }
 
       return $tpl;
    }
 
-   // returns the full path to a given relative template path, if it exists
-   private static function get_template($tpl) 
+   // smarty get template
+   // provides the template source to smarty if smarty cannot locate it
+   // called when [<include>] is used in a template
+   static function smarty_get_template($resource_type, $resource_name, &$template_source, &$template_timestamp, &$smarty_obj)
    {
-		$path = self::$tpl_root . $tpl;
-
-      if (is_readable($path))
+      if($resource_type == 'file')
       {
+         $module = $smarty_obj->get_template_vars('SM_MODULE');
+         $tpl = self::get_template($resource_name, $module);
+
+         if($tpl == NULL)
+            return false;
+
+         $fh = fopen($tpl, 'r');
+         $template_source = fread($fh, filesize($tpl));
+
+         return true;
+      }
+   }
+
+   // returns the full path to the specified template, if the template can be 
+   // found
+   private static function get_template($template, $module = NULL) 
+   {
+      // look first for a template under the module's template directory
+      $tpl = $module . '/' . $template;
+      $path = self::$tpl_root . $tpl;
+      
+      // if a template is not found, search for a partial template
+      if(!self::check_template($path))
+      {
+         $tpl = 'partials/' . $template;
+         $path = self::$tpl_root . $tpl;
+      }
+
+      if(self::check_template($path))
 			return $path;
-		}
-		return NULL;
-	}
+      return NULL;
+   }
+
+   // checks if a template is valid
+   private static function check_template($template_path)
+   {
+      if(is_readable($template_path))
+         return true;
+      return false;
+   }
 
 }
 
